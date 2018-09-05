6bd4da03b69bc941b5269729e9d639b45d2fae81
mathsleg <42952879+mathsleg@users.noreply.github.com>
2018-09-04 20:28:22 -0400

    Add files via upload

--- action-addition.py
+++ action-addition.py
@@ -48,5 +48,5 @@ def action_wrapper(hermes, intentMessage, conf):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("mguyleg getaddt", subscribe_intent_callback) \
+        h.subscribe_intent("mguyleg:getaddt", subscribe_intent_callback) \
 .start()
